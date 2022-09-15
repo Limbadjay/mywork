e51e7ab5ee99bfd711e1954e4f67cadf8a07c083
Limbadjay <107167543+Limbadjay@users.noreply.github.com>
2022-09-15 10:52:46 +0530

    react

---
+++ Reactjs/app1/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let number1 = 10;
+let number2 = 4;
+
+let output = <div className='container'><div className='row'><div className='col-12'><h1>hello recatJS</h1></div></div></div>
+root.render(output);
--- javascript/js/myscript.js
+++ javascript/js/myscript.js
@@ -1,7 +1,23 @@
-console.log("javascript is work...");
-console.log("hello brother");
-let a, b;
-a=10;
-b=20;
-c=a+b;
-console.log(c);
+/*var message = prompt("what is your name");
+document.getElementById("title").innerHTML = message;
+
+var message2 = prompt("what is your address");
+document.getElementById("address").innerHTML = message2;
+
+var city = document.getElementById("city").innerHTML;
+alert(city);*/
+
+
+function calculate_area()
+{
+    var length,width;
+    length = document.getElementById("length");
+    width = document.getElementById("width");
+    console.log(length + " " + width);
+
+    var area = parseInt(length) * parseInt(width);
+    console.log(area);
+    return document.getElementById("output").innerHTML = 'output is' + area;
+}
+
+
